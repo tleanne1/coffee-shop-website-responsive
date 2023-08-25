e275b2d36df6023d1538b72f9e9ab4b7fb66fdbd
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-08-24 20:41:59 -0500

    add search js and media queries

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
 
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
 let cartItem = document.querySelector(".cart-items-container");
 
 document.querySelector("#cart-btn").onclick = () => {
